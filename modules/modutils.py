4632c8a1f240d664c0876b49b9ef0fc46aed2780
asciimoo <asciimoo@gmail.com>
2010-07-19 14:07:40 +0200

    [enh] useful functions for modules moved to separate file -> modutils

---
+++ modules/modutils.py
@@ -0,0 +1,16 @@
+
+
+def splitString(s, n): 
+    return [s[i:i+n] for i in xrange(0, len(s), n)]
+
+def hexStringDecode(s):
+    return ''.join(map(unichr, map((lambda y: int(y, 16)), splitString(s, 2))))
+
+class ModuleStorage:
+    "simple storage class"
+    def __init__(self, value=[''], dtype='', complete=False, notes='', relevance=10):
+        self.value      = value
+        self.complete   = complete
+        self.dtype      = dtype
+        self.notes      = notes
+        self.relevance  = relevance
