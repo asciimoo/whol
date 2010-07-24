3b9d0fe3632bfee8b33c539bd577d42187b2f6bf
asciimoo <asciimoo@gmail.com>
2010-07-24 20:25:34 +0200

    [mod] protocol names merged to modules

--- modules/mod_ftp.py
+++ modules/mod_ftp.py
@@ -1,5 +1,7 @@
 
 FILTER_EXPRESSION='ftp'
 
+PROTO_NAME={'ftp'  : 'File Transfer Protocol'}
+
 def parse(c):
     return []
--- modules/mod_pop.py
+++ modules/mod_pop.py
@@ -1,6 +1,7 @@
 
 FILTER_EXPRESSION='pop.request.command == "PASS" or pop.request.command == "USER"'
 
+PROTO_NAME={'pop'  : 'Post Office Protocol'}
 
 def parse(c):
     return []
