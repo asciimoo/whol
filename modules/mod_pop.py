c04253543e693b8c03236f83f1ab6ddd75ee9589
asciimoo <asciimoo@gmail.com>
2010-07-25 22:02:25 +0200

    [enh] POP3 module added

--- modules/mod_pop.py
+++ modules/mod_pop.py
@@ -1,7 +1,17 @@
+from modutils import hexStringDecode, ModuleStorage
 
 FILTER_EXPRESSION='pop.request.command == "PASS" or pop.request.command == "USER"'
 
 PROTO_NAME={'pop'  : 'Post Office Protocol'}
 
-def parse(c):
+def parse(protos):
+    # pop.request.parameter
+    t = ''
+    for p in protos[0].firstChild.childNodes:
+        if p.attributes['name'].value == 'pop.request.command':
+            t = hexStringDecode(p.attributes['value'].value)
+            continue
+        if p.attributes['name'].value == 'pop.request.parameter':
+            return [ModuleStorage(value=[hexStringDecode(p.attributes['value'].value)], complete=False, dtype='POP_%s' % t, notes=' - ', relevance=10)]
+
     return []
