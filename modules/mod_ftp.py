89c57cb69a5e616e4543b9408679a674d70bc2c4
asciimoo <asciimoo@gmail.com>
2010-07-27 18:21:50 +0200

    [enh] ftp module added

--- modules/mod_ftp.py
+++ modules/mod_ftp.py
@@ -1,7 +1,13 @@
+from modutils import hexStringDecode, ModuleStorage
 
-FILTER_EXPRESSION='ftp'
+FILTER_EXPRESSION='ftp.request == true'
 
 PROTO_NAME={'ftp'  : 'File Transfer Protocol'}
 
-def parse(c):
+def parse(protos):
+    for p in protos[0].childNodes:
+        if p.attributes['show'].value.startswith('PASS '):
+            return [ModuleStorage(value=[hexStringDecode(p.childNodes[1].attributes['value'].value)], complete=False, dtype='FTP_PASS', notes=' - ', relevance=10)]
+        if p.attributes['show'].value.startswith('USER '):
+            return [ModuleStorage(value=[hexStringDecode(p.childNodes[1].attributes['value'].value)], complete=False, dtype='FTP_USER', notes=' - ', relevance=10)]
     return []
