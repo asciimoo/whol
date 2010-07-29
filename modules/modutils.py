0aa4c624b4cafd5af8e5d4fedb56f7c8028557df
asciimoo <asciimoo@gmail.com>
2010-07-29 19:32:05 +0200

    [mod][fix] minor changes

--- modules/modutils.py
+++ modules/modutils.py
@@ -4,7 +4,7 @@ def splitString(s, n):
     return [s[i:i+n] for i in xrange(0, len(s), n)]
 
 def hexStringDecode(s):
-    return ''.join(map(unichr, map((lambda y: int(y, 16)), splitString(s, 2))))
+    return ''.join(map(chr, map((lambda y: int(y, 16)), splitString(s, 2))))
 
 class ModuleStorage:
     "Simple storage class"
@@ -19,4 +19,4 @@ class ModuleStorage:
         return u'Value: "%s"\n\tComplete: %s, Notes: %s, Relevance: %f, Type: %s' % (self.value, str(self.complete), self.notes, self.relevance, self.dtype)
 
     def __str__(self):
-        return u'Value: "%s"\n\tComplete: %s, Notes: %s, Relevance: %f' % (self.value, str(self.complete), self.notes, self.relevance)
+        return u'Value: "%s"\n\tRelevance: %f' % (self.value, self.relevance)
