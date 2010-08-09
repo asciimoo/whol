7495eae4e8c33da83eefded2b8895b6496b8a130
asciimoo <asciimoo@gmail.com>
2010-08-09 19:20:10 +0200

    [enh] verification support added

--- modules/mod_pop.py
+++ modules/mod_pop.py
@@ -1,12 +1,16 @@
 from modutils import hexStringDecode, ModuleStorage
 
-FILTER_EXPRESSION='pop.request.command == "PASS" or pop.request.command == "USER"'
+FILTER_EXPRESSION='pop.request.command == "PASS" or pop.request.command == "USER" or pop.response'
 
 PROTO_NAME={'pop'  : 'Post Office Protocol'}
 
 def parse(protos):
     # pop.request.parameter
     t = ''
+    if protos[0].firstChild.attributes['name'].value == 'pop.response':
+        if protos[0].firstChild.attributes['showname'].value.startswith('+OK logged in.'):
+            return [ModuleStorage(value={'POP_VERIF': ''}, complete=False, notes=' - ', relevance=10, verification=True)]
+
     for p in protos[0].firstChild.childNodes:
         if p.attributes['name'].value == 'pop.request.command':
             t = hexStringDecode(p.attributes['value'].value)
