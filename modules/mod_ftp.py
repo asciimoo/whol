c3fdad5f5313c2e851c2d3f3451b73bb34ea2107
Adam Tauber <asciimoo@gmail.com>
2010-08-29 20:30:17 +0200

    [mod] username to user

--- modules/mod_ftp.py
+++ modules/mod_ftp.py
@@ -5,9 +5,11 @@ FILTER_EXPRESSION='ftp'
 PROTO_NAME={'ftp'  : 'File Transfer Protocol'}
 
 def parse(protos):
+    if protos[0].childNodes[2].attributes['show'].value.startswith('230 OK'):
+        return [ModuleStorage(value={'FTP_VERIF': ''}, complete=False, notes=' - ', relevance=10, verification=True)]
     for p in protos[0].childNodes:
         if p.attributes['show'].value.startswith('PASS '):
-            return [ModuleStorage(value={'password': hexStringDecode(p.childNodes[1].attributes['value'].value)}, complete=False, notes=' - ', relevance=10)]
+            return [ModuleStorage(value={'pass': hexStringDecode(p.childNodes[1].attributes['value'].value)}, complete=False, notes=' - ', relevance=10)]
         if p.attributes['show'].value.startswith('USER '):
-            return [ModuleStorage(value={'username': hexStringDecode(p.childNodes[1].attributes['value'].value)}, complete=False, notes=' - ', relevance=10)]
+            return [ModuleStorage(value={'user': hexStringDecode(p.childNodes[1].attributes['value'].value)}, complete=False, notes=' - ', relevance=10)]
     return []
